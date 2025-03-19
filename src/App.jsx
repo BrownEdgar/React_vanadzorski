b9f3d8bbec22ea388148166132f4fa41b3e05cd0
BrownEdgar <-g>
2025-03-19 12:39:23 +0400

    Fetch posts data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Posts from './components/Posts/Posts';
 
 function App() {
 
   return (
-    <div className='App'></div>
+    <div className='App'>
+      <h1>Our Posts</h1>
+      <Posts />
+    </div>
   )
 }
 
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+
+  async function fetchData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+    const data = await response.json()
+    setPosts(data)
+  }
+
   return <div>
     <h1>Posts ok</h1>
+    {
+      posts.map(({ title, id }) => {
+        return <h2 key={id}>{title}</h2>
+      })
+    }
+    <button onClick={fetchData}>get Posts</button>
   </div>;
 }
 
