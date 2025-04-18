9e34f41d7943e0a942d293332b9ae9ef8588fe44
BrownEdgar <43911617+BrownEdgar@users.noreply.github.com>
2025-04-18 11:01:58 +0400

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import React from 'react'
-import "./app.css"
 
-const Cars = () => {
-  return (
-    <div className="cars">
-      <h2>Cars</h2>
-      <form>
-        <label htmlFor="name">name</label>
-        <input type="text" placeholder="enter name" name="name" />
+import Form from './components/Form'
+import "./App.css"
 
-        <label htmlFor="surname">surname</label>
-        <input type="text" placeholder="enter surname" name="surname" />
 
-        <label htmlFor='email'>email</label>
-        <input type="email" placeholder="enter email" name="email" />
-
-        <label htmlFor='password'>password</label>
-        <input type="password" placeholder="enter password" name="password" />
-
-        <button type="submit">send</button>
-      </form>
-    </div>
-  )
-}
+const Cars = () => (
+  <div className="cars">
+    <h2>Cars</h2>
+    <Form />
+  </div>
+)
 export default Cars
