ff9d342b85e8c6a9cefc312841776542ed886afc
BrownEdgar <43911617+BrownEdgar@users.noreply.github.com>
2025-05-07 12:59:18 +0400

    redux part 2

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from "../features/counter/counterSlice";
 import postsSlice from "../features/posts/postsSlice";
+import twettsSlice from "../features/twetts/twettsSlice";
 
 
 const store = configureStore({
   reducer: {
     counter: counterSlice,
-    posts: postsSlice
+    posts: postsSlice,
+    tweets: twettsSlice
   }
 })
 export default store
